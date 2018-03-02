388e7c4e2ed501044a4228772ca28eca2b850d21
Mike Haydan <misha.gajdan@gmail.com>
2018-03-02 11:23:18 +0200

    update files postition

---
+++ MVP+Tests/Scenes/UsersList/UserListPresenter.swift
@@ -0,0 +1,9 @@
+//
+//  UserListPresenter.swift
+//  MVP+Tests
+//
+//  Created by Mike Haydan on 02/03/2018.
+//  Copyright © 2018 Mike Haydan. All rights reserved.
+//
+
+import Foundation
---
+++ MVP+Tests/Scenes/UsersList/UserListsViewController.swift
@@ -0,0 +1,31 @@
+//
+//  UserListsViewController.swift
+//  MVP+Tests
+//
+//  Created by Mike Haydan on 01/03/2018.
+//  Copyright © 2018 Mike Haydan. All rights reserved.
+//
+
+import UIKit
+
+class UserListsViewController: UIViewController {
+
+    //MARK: - Properties
+    
+    
+    
+    //MARK: - LifeCycle
+    
+    
+    
+    //MARK: - IBActions
+    
+    
+    
+    //MARK: - Private
+    
+    
+    
+    //MARK: - Public
+
+}
